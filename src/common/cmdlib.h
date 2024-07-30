72a3f6a0dbdff70f652efc72895f87c42d28bda9
Gaspar <gasparguidopavon@gmail.com>
2024-07-30 17:27:26 -0300

    Cleanup: cmdlib.h

--- src/common/cmdlib.h
+++ src/common/cmdlib.h
@@ -10,19 +10,16 @@
 // tool specific settings below only mean a recompile of the tool affected
 
 //=====================================================================
+
+#include <unistd.h>
+#include "win32fix.h"
+
 #ifdef __MINGW32__
 #include <io.h>
 #endif
 
 #ifdef SYSTEM_POSIX
 #include <sys/time.h>
-#endif
-#include <unistd.h>
-
-#include "win32fix.h"
-
-#ifdef SYSTEM_WIN32
-#define HLCSG_GAMETEXTMESSAGE_UTF8 //--vluzacn
 #endif
 
 #define MODIFICATIONS_STRING "Submit detailed bug reports to (github.com/seedee/SDHLT/issues)\n"
@@ -55,34 +52,29 @@
 #ifdef SYSTEM_WIN32
 #define SYSTEM_SLASH_CHAR '\\'
 #define SYSTEM_SLASH_STR "\\"
+#define HLCSG_GAMETEXTMESSAGE_UTF8 //--vluzacn
 #endif
+
 #ifdef SYSTEM_POSIX
 #define SYSTEM_SLASH_CHAR '/'
 #define SYSTEM_SLASH_STR "/"
-#endif
-
-// the dec offsetof macro doesn't work very well...
-#define myoffsetof(type, identifier) ((size_t) & ((type *)0)->identifier)
-#define sizeofElement(type, identifier) (sizeof((type *)0)->identifier)
-
-#ifdef SYSTEM_POSIX
 extern auto strupr(char *string) -> char *;
 extern auto strlwr(char *string) -> char *;
 #endif
+
 extern bool CDECL FORMAT_PRINTF(3, 4) safe_snprintf(char *const dest, const size_t count, const char *const args, ...);
 extern auto safe_strncpy(char *const dest, const char *const src, const size_t count) -> bool;
 extern auto safe_strncat(char *const dest, const char *const src, const size_t count) -> bool;
 extern auto TerminatedString(const char *buffer, const int size) -> bool;
 
 extern auto FlipSlashes(char *string) -> char *;
-
-extern auto I_FloatTime() -> double;
-
 extern void DefaultExtension(char *path, const char *extension);
 extern void StripExtension(char *path);
 
 extern void ExtractFile(const char *const path, char *dest);
 extern void ExtractFilePath(const char *const path, char *dest);
+
+extern auto I_FloatTime() -> double;
 
 extern auto LittleShort(short l) -> short;
 extern auto LittleLong(int l) -> int;
